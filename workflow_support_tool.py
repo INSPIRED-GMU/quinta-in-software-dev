e442581253728cef26c51d4f0b30488dbc0ca954
Jibiana Jakpor <14021192+gbahnah@users.noreply.github.com>
2021-07-19 17:03:21 -0700

    Create workflow_support_tool.py

---
+++ workflow_support_tool.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import ttk
+
+class App:
+    def __init__(self, root):
+        root.title("QUINTA Workflow Suport Tool")
+
+        mainframe = ttk.Frame(root) # TODO: add padding
+
+class StartPage: # TODO: Create the start page 
+    pass
+
+class SessionHome:
+    pass
+
+class DesignReflectionPage:
+    pass
