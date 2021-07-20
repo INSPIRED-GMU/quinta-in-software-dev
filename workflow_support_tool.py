61bad5c54e836cf20e997e7a7dea1abc58481a6d
Jibiana Jakpor <14021192+gbahnah@users.noreply.github.com>
2021-07-19 17:42:32 -0700

    Start the start page

--- workflow_support_tool.py
+++ workflow_support_tool.py
@@ -3,12 +3,19 @@ from tkinter import ttk
 
 class App:
     def __init__(self, root):
-        root.title("QUINTA Workflow Suport Tool")
+        root.title('QUINTA Workflow Suport Tool')
 
         mainframe = ttk.Frame(root) # TODO: add padding
+        
 
 class StartPage: # TODO: Create the start page 
-    pass
+    def __init__(self, root, mainframe):
+        design = ttk.Labelframe(mainframe, text='Design')
+        data_collection = ttk.Labelframe(mainframe, text='Data Collection')
+        explore = ttk.Labelframe(mainframe, text='Explore')
+        model = ttk.Labelframe(mainframe, text='Model')
+        interpret = ttk.Labelframe(mainframe, text='Interpret')
+        mainframe.grid()
 
 class SessionHome:
     pass
