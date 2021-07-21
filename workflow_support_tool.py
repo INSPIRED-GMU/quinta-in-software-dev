00a40412b597d0aa120d416d4ce25c545b636fb2
Jibiana Jakpor <14021192+gbahnah@users.noreply.github.com>
2021-07-21 15:28:51 -0700

    Added Design labelframe with button to startpage

--- workflow_support_tool.py
+++ workflow_support_tool.py
@@ -1,24 +1,49 @@
 from tkinter import *
 from tkinter import ttk
 
-class App:
-    def __init__(self, root):
-        root.title('QUINTA Workflow Suport Tool')
-
-        mainframe = ttk.Frame(root) # TODO: add padding
+class App(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.grid(column=0, row=0)
+        Label(self, text='Hello').grid(column=0, row=0)
+        ttk.Labelframe(self, text='Design').grid(column=0, row=0)
+        ttk.Labelframe(self, text='Data Collection').grid(column=1, row=0)
+        ttk.Labelframe(self, text='Cleaning').grid(column=2, row=0)
+        ttk.Labelframe(self, text='Explore').grid(column=0, row=1)
+        ttk.Labelframe(self, text='Model').grid(column=1, row=1)
+        ttk.Labelframe(self, text='Interpret').grid(column=2, row=1)
+        StartPage(self)
         
 
 class StartPage: # TODO: Create the start page 
-    def __init__(self, root, mainframe):
-        design = ttk.Labelframe(mainframe, text='Design')
-        data_collection = ttk.Labelframe(mainframe, text='Data Collection')
-        explore = ttk.Labelframe(mainframe, text='Explore')
-        model = ttk.Labelframe(mainframe, text='Model')
-        interpret = ttk.Labelframe(mainframe, text='Interpret')
-        mainframe.grid()
+    def __init__(self, parent):
+        start_frame = Frame(parent)
+        start_frame.grid(column=0, row=0)
+        design = ttk.Labelframe(start_frame, text='Design')
+        data_collection = ttk.Labelframe(start_frame, text='Data Collection')
+        cleaning = ttk.Labelframe(start_frame, text='Cleaning')
+        explore = ttk.Labelframe(start_frame, text='Explore')
+        model = ttk.Labelframe(start_frame, text='Model')
+        interpret = ttk.Labelframe(start_frame, text='Interpret')
+
+        design.grid(column=0, row=0)
+        data_collection.grid(column=1, row=0)
+        cleaning.grid(column=2, row=0)
+        explore.grid(column=0, row=1)
+        model.grid(column=1, row=1)
+        interpret.grid(column=2, row=1)
+
+        design_reflection = ttk.Button(design, text='Reflection')
+        design_reflection.grid(column=1, row=0)
+
 
 class SessionHome:
     pass
 
 class DesignReflectionPage:
     pass
+
+root = Tk()
+root.title = 'QUINTA Workflow Support Tool'
+App(root)
+root.mainloop()
