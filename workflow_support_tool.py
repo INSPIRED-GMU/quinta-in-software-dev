dd30d4d8bf0ca684da670d9c7d1fa1de771f079a
Jibiana Jakpor <14021192+gbahnah@users.noreply.github.com>
2021-07-21 15:42:30 -0700

    Moved UI elements from StartPage to SessionHome

--- workflow_support_tool.py
+++ workflow_support_tool.py
@@ -5,26 +5,22 @@ class App(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.grid(column=0, row=0)
-        Label(self, text='Hello').grid(column=0, row=0)
-        ttk.Labelframe(self, text='Design').grid(column=0, row=0)
-        ttk.Labelframe(self, text='Data Collection').grid(column=1, row=0)
-        ttk.Labelframe(self, text='Cleaning').grid(column=2, row=0)
-        ttk.Labelframe(self, text='Explore').grid(column=0, row=1)
-        ttk.Labelframe(self, text='Model').grid(column=1, row=1)
-        ttk.Labelframe(self, text='Interpret').grid(column=2, row=1)
-        StartPage(self)
+        SessionHome(self)
         
 
-class StartPage: # TODO: Create the start page 
+class StartPage:
+    pass
+
+class SessionHome:
     def __init__(self, parent):
-        start_frame = Frame(parent)
-        start_frame.grid(column=0, row=0)
-        design = ttk.Labelframe(start_frame, text='Design')
-        data_collection = ttk.Labelframe(start_frame, text='Data Collection')
-        cleaning = ttk.Labelframe(start_frame, text='Cleaning')
-        explore = ttk.Labelframe(start_frame, text='Explore')
-        model = ttk.Labelframe(start_frame, text='Model')
-        interpret = ttk.Labelframe(start_frame, text='Interpret')
+        session = Frame(parent)
+        session.grid(column=0, row=0)
+        design = ttk.Labelframe(session, text='Design')
+        data_collection = ttk.Labelframe(session, text='Data Collection')
+        cleaning = ttk.Labelframe(session, text='Cleaning')
+        explore = ttk.Labelframe(session, text='Explore')
+        model = ttk.Labelframe(session, text='Model')
+        interpret = ttk.Labelframe(session, text='Interpret')
 
         design.grid(column=0, row=0)
         data_collection.grid(column=1, row=0)
@@ -35,10 +31,6 @@ class StartPage: # TODO: Create the start page
 
         design_reflection = ttk.Button(design, text='Reflection')
         design_reflection.grid(column=1, row=0)
-
-
-class SessionHome:
-    pass
 
 class DesignReflectionPage:
     pass
