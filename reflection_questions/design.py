03fce4fc12533028d1c34d1069b0a2604d5bcc31
Jibiana Jakpor <14021192+gbahnah@users.noreply.github.com>
2021-07-22 15:16:06 -0700

    Create package with reflection questions in files as constants

---
+++ reflection_questions/design.py
@@ -0,0 +1,5 @@
+PURPOSE = 'What is your research about?'
+SOCIAL_CONTEXT = 'What is the social and historical context of the inequality of social issues your work might affect?'
+MOTIVE = 'How do you personally benefit from this project?'
+WHO_HARM = 'If this research is not done carefully, who might it harm?'
+WORST_CASE = 'What is the worst thing that could happen to someone because of this model?'
