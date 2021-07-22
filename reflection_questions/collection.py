ce337bbf1fa849a3350ee4f96d27dd7ff20a3ca4
Jibiana Jakpor <14021192+gbahnah@users.noreply.github.com>
2021-07-22 16:16:10 -0700

    Add questions about representation to collection.py

--- reflection_questions/collection.py
+++ reflection_questions/collection.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-
 # These possible attributes are from page 44 of Alicia Boyd's dissertation
 # from a list that was not meant to be exhaustive
 possible_attributes = tk.StringVar(value=['Race', 'Class', 'Gender',
@@ -10,3 +9,16 @@ possible_attributes = tk.StringVar(value=['Race', 'Class', 'Gender',
 def listbox_attributes(parent):
     """Display a listbox of attributes included in the data"""
     return Listbox(parent, listvariable=possible_attributes)
+
+INTERSECTIONAL_DATA = 'Are your data intersectional? Why or why not? Remember' \
+    'that the experiences associated with one identity a person has' \
+    'are not necessarily independent of or separate from another identity of' \
+    'that person. How well can your dataset reflect this?'
+
+MOST_REPPED = 'Think about the intersectional identities that are the most ' \
+    'represented. Who is being centered in the data? Why? How might this ' \
+    'affect the equity of your analysis?'
+
+LEAST_REPPED = 'Think about the intersectional identities that are the least ' \
+    'represented. Who is being centered in the data? Why? How might this ' \
+    'affect the equity of your analysis?'
