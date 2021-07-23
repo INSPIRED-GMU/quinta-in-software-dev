d72bcebb9e30102c62cfb07b376603830406afd4
Jibiana Jakpor <14021192+gbahnah@users.noreply.github.com>
2021-07-23 11:35:40 -0700

    Add PhaseMenu class

--- oop_workflow_support_tool.py
+++ oop_workflow_support_tool.py
@@ -6,6 +6,7 @@ import reflection_questions.design as drq
 class Session(ttk.Frame):
     """The main session screen."""
     def __init__(self, parent):
+        self.parent = parent
         ttk.Frame.__init__(self, parent)
         design = ttk.Labelframe(self, text='Design')
         data_collection = ttk.Labelframe(self, text='Data Collection')
@@ -20,6 +21,10 @@ class Session(ttk.Frame):
         explore.grid(column=0, row=1)
         model.grid(column=1, row=1)
         interpret.grid(column=2, row=1)
+
+    def switch_to_design(self):
+        self.grid_forget()
+        DesignReflectionScreen(self.parent).grid(column=0, row=0)
         
 class DesignReflectionScreen(ttk.Frame):
     """A screen of reflection questions for the Design phase."""
@@ -35,11 +40,24 @@ class TextQuestion:
         self.textbox = tk.Text(parent)
 
     def show(self, c, r):
-        """Show the question and answer on screen"""
+        """Show the question and answer on screen."""
         self.label.grid(column=c, row=r)
         self.textbox.grid(column=c, row=r+1)
         
+class PhaseMenu(ttk.Labelframe):
+    """A menu for the phase."""
+    def __init__(self, parent, buttons):
+        ttk.Labelframe.__init__(self, parent)
+        for i, button_text, frame in buttons:
+            ttk.Button(self, text=button_text, command=lambda parent, self: replace_frame(parent, self)).grid(column=0, row=i)
+
+
+def replace_frame(frame1, frame2):
+    """Replace frame1 with frame2 on screen."""
+    frame1.grid_forget()
+    frame2.grid(column=0, row=0)
+
 root = tk.Tk()
 session = Session(root).grid(column=0,  row=0)
-DesignReflectionScreen(session).grid(column=0, row=0)
+
 root.mainloop()
