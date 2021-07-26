992d55896d72715362f95096068c0161c7bc1217
Jibiana Jakpor <14021192+gbahnah@users.noreply.github.com>
2021-07-26 10:38:46 -0700

    Separate reading of data from plotting

--- data_collection_representation_analysis_screen.py
+++ data_collection_representation_analysis_screen.py
@@ -16,26 +16,36 @@ mainframe.grid()
 filename = tk.StringVar()
 tk.Button(mainframe, text='Load dataset', command= lambda: filename.set(
     tk.filedialog.askopenfilename())).grid()
+
+# Create radiobuttons for file formats
 format = tk.StringVar()
 file_format_radio_buttons = {file_format: ttk.Radiobutton(mainframe,
  text=file_format, variable=format, value=file_format) for file_format in ['CSV', 'TSV', 'JSON', 'SQL']}
-# TODO: make the file buttons actually change file format
 for i, ff in enumerate(file_format_radio_buttons, 3):
     file_format_radio_buttons[ff].grid(column=0, row=i)
 
-class PlotRep:
+def read_data(file_path, format_string_var):
+    """Return a pandas.DataFrame that reads data in the right format."""
+    format = format_string_var.get()
+    if format == 'CSV':
+        read_func = pd.read_csv
+    elif format == 'TSV':
+        read_func = pd.read_table
+    elif format == 'JSON':
+        read_func = pd.read_json
+    elif format == 'SQL':
+        read_func = pd.read_sql
+    return read_func(file_path)
+
+
+
+class PlotRepPie:
     """Plot intersectional representation."""
-    def __init__(file_path, col_list, format_string_var, parent):
+    def __init__(self, df, col_list, format_string_var, parent):
         format = format_string_var.get()
-        if format == 'CSV':
-            read_func = pd.read_csv
-        elif format == 'TSV':
-            read_func = pd.read_table
-        elif format == 'JSON':
-            read_func = pd.read_json
-        elif format == 'SQL':
-            read_func = pd.read_sql
-        deu.intersectional_rep_pie(read_func(file_path), col_list, parent).grid(column=4, row=2)
+        self.pie = deu.intersectional_rep_pie(df, col_list,
+                                              parent).get_tk_widget() 
+         
 
     
 
